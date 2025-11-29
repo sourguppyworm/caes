dca497b2d2ba89c5b016085ebe8213a97aa6618c
guppy <38512219+sourguppyworm@users.noreply.github.com>
2025-11-29 01:35:51 -0500

    Remove leftover comments

--- caesar.rb
+++ caesar.rb
@@ -1,10 +1,5 @@
-# uugughhbghghhhh
-# convert to ord whatever
-# if it exceeds z subtract 26 from it
-# spit out result problem solved
-
 def caesar_cipher(string, shift = 0)
-  char_arr = string.split("") # gsub(/[^A-Za-z\s]+/, "").bytes
+  char_arr = string.split("")
   result = char_arr.collect do |char|
     if char.between?("a", "z")
       convert_char = (char.ord - "a".ord + shift) % 26 + "a".ord 
